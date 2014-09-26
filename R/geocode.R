be2deb9c91204addf62833d91421865403af4f75
tarakc02 <tarakc02@users.noreply.github.com>
2014-09-25 23:36:53 -0700

    Add Mapquest API to geocoding options
    Also add a save function to export standalone maps.

--- R/geocode.R
+++ R/geocode.R
@@ -1,9 +1,81 @@
+#' @import jsonlite
+geocode_mapquest_batch <- function(addresses, 
+                                   mapquest_key) {
+    # build a search url
+    geo_url <- "http://open.mapquestapi.com/geocoding/v1/batch/"
+    loclist = paste("location=", addresses, sep="", collapse="&")
+    
+    query <- list(key = mapquest_key,
+                  maxResults = 1L,
+                  thumbMaps = "false",
+                  Format = "kvp")
+    query <- paste(names(query), query, sep="=", collapse="&")
+    query <- paste(query, loclist, sep="&")
+    con <- paste(geo_url, query, sep="?")
+    
+    # get and parse results
+    resp <- readLines(con)
+    resp <- jsonlite::fromJSON(resp)
+    
+    # fill in blank data frames with NA to keep lat/lng frames same dimensions
+    # as the original address vector
+    latlng <- lapply(resp$results$locations, 
+                     function(x) x$latLng[,c("lat", "lng")])
+    latlng[sapply(latlng, is.null)] <- data.frame(lat = NA_real_, lng = NA_real_)
+    latlng <- do.call("rbind", latlng)
+    
+    # output data frame of address, latitude, longitude
+    outp <- cbind(resp$results$providedLocation, latlng)
+    names(outp) <- c("address", "latitude", "longitude")
+    outp
+}
+
+#' Geocode addresses using the Mapquest Open Geocoding API
+#' 
+#' You will need an API key to use this function. You can request one from
+#' \link{http://developer.mapquest.com/web/products/open/geocoding-service}.
+#' 
+#' @param addresses A character vector of addresses
+#' @param mapquest_key Your API key
+#' @param batch Reserved for future use
+#' @param batch_size Addresses will be geocoded using the batch geocoding 
+#' service. The \code{batch_size} controls how large each batch should be.
+geocode_mapquest <- function(addresses, 
+                             mapquest_key, 
+                             batch = TRUE, 
+                             batch_size = 100L) {
+    
+    # returns a list of vectors w/ at most batch_size elements each
+    batched_addresses <- chunk(addresses, batch_size)
+    num_batches <- length(batched_addresses)
+    
+    geocodes <- vector("list", num_batches)
+    for (b in 1:num_batches) {
+        geocodes[[b]] <- geocode_mapquest_batch(batched_addresses[[b]],
+                                                mapquest_key)
+        Sys.sleep(1)
+    }
+    do.call("rbind", geocodes)
+}
+
+#' Geocode addresses using the Google Maps API
+#' 
+#' This is \code{geocode} with \code{src = "google"}. Note that by using this 
+#' function, you're agreeing to the Google Maps API terms of service. See 
+#' \link{https://developers.google.com/maps/terms#section_10_12}.
+#' 
+#' @note You will be limited to 2,500 addresses per 24-hour period.
+#' 
+#' @param addresses A character vector of addresses
+#' @return A data frame with three columns: \code{address}, \code{latitude},
+#' and \code{longitude}.
+#' 
 #' @import RgoogleMaps
 geocode_google <- function(addresses) {
     geocodes <- vector("list", length=length(addresses))
     
     for (a in 1:length(addresses)) {
-        if(!(a %% 5))  Sys.sleep(1)
+        if(!(a %% 5L))  Sys.sleep(1L)
         geocodes[[a]] <- RgoogleMaps::getGeoCode(addresses[a])
         names(geocodes)[a] <- addresses[a]
     }
@@ -20,14 +92,17 @@ geocode_google <- function(addresses) {
 #' Get geo-codes from addresses
 #' 
 #' @return A \code{data.frame} with three columns: \code{address}, 
-#' \code{lat}, and \code{lon}
+#' \code{latitude}, and \code{longitude}
 #' 
 #' @param addresses A character vector of addresses
-#' @param src Which geo-coding API to use? Currently "google" is the only 
-#' available option
+#' @param src Which geo-coding API to use? ("google" or "mapquest")
+#' 
+#' @seealso \code{\link{geocode_google}}, \code{\link{geocode_mapquest}}
 #' @export
-geocode <- function(addresses, src = "google") {
-    geocode_google(addresses)
+geocode <- function(addresses, src = "google", ...) {
+    switch(src, 
+           google = geocode_google(addresses),
+           mapquest = geocode_mapquest(addresses, ...))
 }
 
 #' Append geocodes to a data frame that includes addresses
@@ -36,7 +111,7 @@ geocode <- function(addresses, src = "google") {
 #' @param cols A vector of names or index numbers of the columns that identify 
 #' the address-related columns in the data. Ideally, these should be in a 
 #' natural order (street, city, state, zip, country).
-#' @param ... Other arguments passed to \code{geocode}
+#' @param ... Other arguments passed to \code{\link{geocode}}
 #' 
 #' @return The original \code{data.frame} with the added columns \code{address},
 #' \code{lat}, and \code{lon}
@@ -46,5 +121,3 @@ append_geocode <- function(df, cols, ...) {
     gc <- geocode(addresses, ...)
     cbind(df, gc[2:3])
 }
-
-
--- R/make_leaflet.R
+++ R/make_leaflet.R
@@ -1,6 +1,13 @@
+#' Rearrange geocoded data to a leaflet map-ready format
+#' 
+#' @param geo_data A data frame that has columns \code{latitude} and 
+#' \code{longitude}
+#' @param popup_columns The names of the columns in the data frame that should 
+#' be used to populate the pop-ups on the map. By default, this will include all 
+#' columns except for the latitude and longitude columns. 
 prep_leaf_data <- function(geo_data, 
                            popup_columns = colnames(geo_data)[!(colnames(geo_data) %in% c("latitude", "longitude"))]) {    
-    
+    geo_data <- geo_data[complete.cases(geo_data),]
     leaf_map_data <- lapply(1:nrow(geo_data), function(x) {
         r <- as.list(geo_data[x,])
         c(r,
@@ -30,6 +37,7 @@ prep_leaf_data <- function(geo_data,
 #' @param map_center A vector witht the latitude and longitude where the map 
 #' should be centered when it is first opened
 #' @param default_zoom The zoom level that the map will be at when first opened
+#' @param ... Other arguments passed on to \code{\link{prep_leaf_data}}
 #' @export
 make_leaflet <- function(map_data, 
                          width = 900,
@@ -62,4 +70,29 @@ make_leaflet <- function(map_data,
     L1$enablePopover(FALSE)
     L1$fullScreen(TRUE)
     L1
+}
+
+#' Save a map you've already made to an HTML file
+#' 
+#' Save a leaflet map to a standalone HTML file. This will output a file that 
+#' you can send to clients or upload. By default, you'll be prompted with a 
+#' file window asking where you want to save the file.
+#' @import rCharts
+#' @import base64enc
+#' @param leafmap The leaflet map you'd like to save
+#' @param location If you know where you'd like to save the file, enter the 
+#' full path (including the name of the file). If you don't enter a location, 
+#' you'll get a graphical prompt to click your way to a save location.
+#' @export
+#' @examples
+#' map_data <- append_geocode(urel)
+#' leaf <- make_leaflet(map_data)
+#' save_leaflet(leaf)  ## will bring up a save file dialog window
+#' # alternatively:
+#' save_leaflet(leaf, "my map.html")
+save_leaflet <- function(leafmap, location = NULL) {
+    if (class(leafmap) != "Leaflet") 
+        stop("This function only works with a leaflet map object")
+    if (is.null(location)) location <- file.choose(TRUE)
+    leafmap$save(location, standalone=TRUE)
 }
